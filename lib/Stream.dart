b8175fb8feaf052edb99eb095aa16a1b738a0906
howkmii <abdelhakim@univ-tiaret.dz>
2021-10-07 22:19:29 +0100

    Stream

--- lib/Stream.dart
+++ lib/Stream.dart
@@ -1,3 +1,5 @@
-void main(List<String> args) {
-  final s1= Stream.periodic(Duration(milliseconds: 500),(a)=>a);
+void main() {
+  final s1= Stream.periodic(Duration(milliseconds: 500),(a)=>a+5);
+  s1.listen((event) {print(event);});
+
 }
