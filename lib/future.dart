e8699387abc06cc9493aae654abcbaef7159eb54
howkmii <abdelhakim@univ-tiaret.dz>
2021-10-07 22:10:57 +0100

    Future delayed

--- lib/future.dart
+++ lib/future.dart
@@ -1,6 +1,9 @@
 void main() {
   final f1  = Future(info);
   f1.then((value) => print('value=$value')).catchError((error)=>print('error=$error'));
+  final f2=Future.value(2);
+  f2.then((value)=>print(value));
+  final f3 = Future.delayed(Duration(seconds: 2));
   print(1);
 }
 double info(){
