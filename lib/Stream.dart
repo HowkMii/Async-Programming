11ccbc5ee1f72d26aae7a6e3cea979908a1aeed0
howkmii <abdelhakim@univ-tiaret.dz>
2021-10-08 14:43:35 +0100

    use three  method cancel pause or onresume

--- lib/Stream.dart
+++ lib/Stream.dart
@@ -1,5 +1,6 @@
 void main() {
   final s1= Stream.periodic(Duration(milliseconds: 500),(a)=>a+5);
-  final sub =s1.listen((event) => print(event));
+  final sub =s1.listen((_)=>_);
   sub.onDone(() => print('Done'));
+  sub.onData((data) { data >10?sub.cancel():print(data);})
 }
