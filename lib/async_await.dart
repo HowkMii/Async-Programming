b26c7b1b743c88edabb1a2dbfc2e439215a2a699
howkmii <abdelhakim@univ-tiaret.dz>
2021-10-09 12:49:31 +0100

    future and stream

--- lib/async_await.dart
+++ lib/async_await.dart
@@ -1,3 +1,12 @@
 void main() {
-  
+    Future.delayed(Duration(seconds: 2),()=>2)
+    .then((value)=>print('value=$value'))
+    .catchError((e)=>print('error=$e'));
+    print(1);
+
+    final s1= Stream.periodic(Duration(milliseconds: 500),(a)=>a);
+    final sub =s1.listen((_)=> _);
+    sub.onData((data) => data>10?sub.cancel():print(data));
+
+
 }
