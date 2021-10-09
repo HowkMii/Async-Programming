1512eb798724b0dc48e3715030ccad3ea3af2312
howkmii <abdelhakim@univ-tiaret.dz>
2021-10-09 13:05:30 +0100

    add catch error simle code

--- lib/async_await.dart
+++ lib/async_await.dart
@@ -4,10 +4,13 @@ void main() async{
     .then((value)=>print('value=$value'))
     .catchError((e)=>print('error=$e'));
     print(1);  */
-    final int value = await Future.delayed(Duration(seconds: 2),()=>2);
-    .then((value)=>print('value=$value'))
-    .catchError((e)=>print('error=$e'));
-    print(1);
+    try{
+       final  value = await Future.delayed(Duration(seconds: 2),()=>2);
+       print('value=$value');
+    } catch(e){
+      print('error=$e');
+    }
+    print('valueeeeeeeeeeeeeee');
 
     final s1= Stream.periodic(Duration(milliseconds: 500),(a)=>a);
     final sub =s1.listen((_)=> _);
