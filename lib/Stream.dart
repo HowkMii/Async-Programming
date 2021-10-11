c41bba9f7d07b0e82c8c43e6dfff6c8e003a9668
howkmii <abdelhakim@univ-tiaret.dz>
2021-10-11 19:13:54 +0100

    _

--- lib/Stream.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:async';
-void main() {
-  final s1= Stream.periodic(Duration(milliseconds: 500),(a)=>a);
-  final s2=s1.where((event) => event%2==0);
-  s2.listen((event) {print(event);});
-}
